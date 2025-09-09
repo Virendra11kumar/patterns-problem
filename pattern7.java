430a21d29a7b5ff1ced0e434fa2968951049ee49
Virendra kumar <virendra91184kumar@gmail.com>
2025-09-09 23:30:38 +0530

    Update pattern7.java

--- pattern7.java
+++ pattern7.java
@@ -1,3 +1,10 @@
+/* Pattern7:-
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+   */
 public class pattern7 {
     public static void main(String[] args) {
         int a=1;
@@ -12,3 +19,4 @@ public class pattern7 {
     }
 }
 }
+
